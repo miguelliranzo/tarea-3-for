ea07701a44e625b2ee26d6de7b9343575df5be0f
miguel antonio liranzo zabala <miguel2012000@hotmail.com>
2024-05-17 20:53:37 -0700

    Add project files.

---
+++ tarea 3 for/Class/Main.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace tarea_3_for.Class
+{
+    public class Main
+    {
+
+        public void Task3() {
+            Console.WriteLine("Tabla de multiplicar del 5 (del 5 al 50):");
+            Console.WriteLine("-----------------------------------------");
+
+       
+            for (int i = 1; i <= 10; i++)
+            {
+               
+                int resultado = 5 * i;
+
+                
+                Console.WriteLine($"5 * {i} = {resultado}");
+            }
+
+        }
+    }
+}
---
+++ tarea 3 for/Program.cs
@@ -0,0 +1,3 @@
+﻿using tarea_3_for.Class;
+Main main = new Main();
+main.Task3();
